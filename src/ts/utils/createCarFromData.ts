d8e85b0066c5530594fa238420b7991564415496
Dwightm1 <artemdwightm1@Noutbuk-Dwightm1.local>
2025-05-01 13:15:39 +0300

    Deleted file 'big-bmw-001.avif' + deleted src/ts/models.ts from repository and added to gitignore

--- src/ts/utils/createCarFromData.ts
+++ src/ts/utils/createCarFromData.ts
@@ -1,6 +1,7 @@
 import { Car } from "../models/Car";
 import { NewCar } from "../models/NewCar";
 import { NewElectricCar } from "../models/NewElectricCar";
+import { UsedCar } from "../models/UsedCar";
 
 
 export function createCarFromData(id: string, data: any): Car {
@@ -9,6 +10,8 @@ export function createCarFromData(id: string, data: any): Car {
             return new NewCar(data.id, data.type, data.model, data.price, data.description, data.specs, data.features, data.imageUrl, data.warrantyYears, data.isRecommended)
         case 'new-electric': 
             return new NewElectricCar(data.id, data.type, data.model, data.price, data.description, data.specs, data.features, data.imageUrl, data.batteryCapacity, data.chargeTime, data.warrantyYears)
+        case 'used-car': 
+            return new UsedCar(data.id, data.type, data.model, data.price, data.description, data.specs, data.features, data.imageUrl, data.mileage, data.isRecommended)
         default: 
         return new Car(data.id, data.type, data.model, data.price, data.description, data.specs, data.features, data.imageUrl, data.isRecommended)
         }
